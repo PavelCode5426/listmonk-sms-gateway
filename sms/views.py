19306e556fb924d58307161ed8a8154627809b02
PavelCode <perezpavel5426@gmail.com>
2025-07-06 10:52:37 -0400

    Update

--- sms/views.py
+++ sms/views.py
@@ -6,6 +6,10 @@ from .serializers import WhatsAppMessageSerializer, SMSMessageSerializer
 class SendMessageMixin(CreateAPIView):
     queryset = models.Message.objects.all()
 
+    def create(self, request, *args, **kwargs):
+        print(request.data)
+        return super(SendMessageMixin, self).create(request, *args, **kwargs)
+
 
 class WhatsAppSendMessages(SendMessageMixin):
     serializer_class = WhatsAppMessageSerializer
